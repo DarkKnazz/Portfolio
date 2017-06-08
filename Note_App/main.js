4aa42ee2886138ed12699cb822a575ce0669aef5
DarkKnazz <zhenyaknazz@gmail.com>
2017-06-08 19:57:11 +0300

    Final version of front-end of my app

--- Note_App/main.js
+++ Note_App/main.js
@@ -4,23 +4,42 @@
 //3 - #00A876
 //4 - #207E62
 //5 - #006D4C
+var countNote = 0;
+
 $(function() {
   $( ".drag1" ).draggable();
 });
 
 $("#submitButton").click(function(){
+  countNote++;
   var numLeft = Math.random() * (850) + 20;
   var numTop = Math.random() * 380 + 90;
   var strLeft = numLeft.toFixed() + "px";
   var strTop = numTop.toFixed() + "px";
   var t = $("<div class='drag1' class='ui-widget-content'></div>");
+  var innerButton = $("<div class='exBut'></div>");
   
-  var data = $("#headerInput").val();
-  $(t).html(data.toUpperCase());
+  $(t).attr("id",countNote.toString());
+  var head_Row = $("#headerInput").val();
+  var main_Text = $("#textInput").val();
+  $(t).html(head_Row.toUpperCase() + "\n" + main_Text);
   $(t).css("left", strLeft);
   $(t).css("top", strTop);
   $("section").append(t);
+  
+  $(innerButton).attr("id", countNote.toString() + "_but");
+  $(innerButton).attr("onclick", "func1(id)");
+  $(t).append(innerButton);
+  
   $(".drag1").draggable();
+  $(".drag1").fadeIn(500);
   $("#headerInput").val('');
   $("#textInput").val('');
 });
+
+function func1(count){
+  var str = "#"+ count.split("_")[0];
+  console.log(str);
+  $(str).fadeOut(500);
+  setTimeout(function(){$(str).remove();}, 1000);
+}
